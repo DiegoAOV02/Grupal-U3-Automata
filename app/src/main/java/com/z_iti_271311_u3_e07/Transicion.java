3a6ebbf70ea72dfd5011fe61816a1a9f27160fcb
Hermayonick-Catalina-Hernandez-Gonzalez <2130036@upv.edu.mx>
2024-11-29 20:34:35 -0600

    Codigo principal

--- app/src/main/java/com/z_iti_271311_u3_e07/Transicion.java
+++ app/src/main/java/com/z_iti_271311_u3_e07/Transicion.java
@@ -1,32 +1,15 @@
 package com.z_iti_271311_u3_e07;
 
 public class Transicion {
-<<<<<<< HEAD
-    private String to;
-    private String from;
-    private int valor;
+    private Estado to;
+    private Estado from;
+    private String valor;
 
-    public Transicion(String from, String to, int valor) {
+    public Transicion(Estado from, Estado to, String valor) {
         this.from = from;
         this.to = to;
         this.valor = valor;
     }
-
-    public String getTo() {
-        return to;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-=======
-    private Estado to;
-    private Estado from;
-    private String valor;
 
     public Estado getFrom() {
         return from;
@@ -51,5 +34,4 @@ public class Transicion {
     public void setValor(String valor) {
         this.valor = valor;
     }
->>>>>>> abddab2da0ba69542d0a3dda7d3a0494839b8656
 }
