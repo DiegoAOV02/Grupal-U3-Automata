a7030288da3068540b2498545d55eba35b1dfc48
Yatzari <2130039@upv.edu.mx>
2024-12-09 13:56:50 -0600

    Transicion

--- app/src/main/java/com/z_iti_271311_u3_e07/Transicion.java
+++ app/src/main/java/com/z_iti_271311_u3_e07/Transicion.java
@@ -1,14 +1,34 @@
 package com.z_iti_271311_u3_e07;
 
+import androidx.annotation.NonNull;
+
+import org.opencv.core.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transicion {
     private Estado to;
     private Estado from;
-    private String valor;
+    private String valor = "0";
+    private Point puntoInicio;
+    private Point puntoFin;
 
-    public Transicion(Estado from, Estado to, String valor) {
+    public Transicion(Estado from, Estado to) {
         this.from = from;
         this.to = to;
-        this.valor = valor;
+    }
+
+    public Transicion(Point puntoInicio, Point puntoFin) {
+        this.puntoInicio = puntoInicio;
+        this.puntoFin = puntoFin;
+    }
+
+    public Transicion(Estado from, Estado to, Point puntoInicio, Point puntoFin) {
+        this.from = from;
+        this.to = to;
+        this.puntoInicio = puntoInicio;
+        this.puntoFin = puntoFin;
     }
 
     public Estado getFrom() {
@@ -33,5 +53,27 @@ public class Transicion {
 
     public void setValor(String valor) {
         this.valor = valor;
+    }
+
+    public Point getPuntoInicio() {
+        return puntoInicio;
+    }
+
+    public void setPuntoInicio(Point puntoInicio) {
+        this.puntoInicio = puntoInicio;
+    }
+
+    public Point getPuntoFin() {
+        return puntoFin;
+    }
+
+    public void setPuntoFin(Point puntoFin) {
+        this.puntoFin = puntoFin;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "De " + from.getNombre() + " a " + to.getNombre() + " con valor " + valor;
     }
 }
